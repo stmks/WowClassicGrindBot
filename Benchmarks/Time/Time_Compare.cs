7db7465fc9a72e297666e3a310cd1484dd4519a1
Xian55 <367101+Xian55@users.noreply.github.com>
2025-02-01 16:02:20 +0100

    Benchmarks: Added Time Comparison.

---
+++ Benchmarks/Time/Time_Compare.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using System.Diagnostics;
+
+namespace Benchmarks.Time;
+
+#pragma warning disable CA1822 
+
+[MemoryDiagnoser]
+public class Time_Compare
+{
+    private readonly static DateTime Now = DateTime.UtcNow;
+    private readonly static long NowTicks = Stopwatch.GetTimestamp();
+
+    [Benchmark]
+    public long DateTimeUtcNow()
+    {
+        return DateTime.UtcNow.Ticks;
+    }
+
+    [Benchmark]
+    public long GetTimeStamp()
+    {
+        return Stopwatch.GetTimestamp();
+    }
+
+    [Benchmark]
+    public long DateTimeUtcNow_Difference()
+    {
+        return (DateTime.UtcNow - Now).Ticks;
+    }
+
+    [Benchmark]
+    public long GetTimeStamp_Difference()
+    {
+        return Stopwatch.GetElapsedTime(NowTicks).Ticks;
+    }
+}
