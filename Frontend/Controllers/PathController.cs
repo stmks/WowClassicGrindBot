d7486353d9fd7b7964c43280a133824f603a8aa6
Xian55 <367101+Xian55@users.noreply.github.com>
2025-04-21 16:53:54 +0200

    Frontend: project has been improved, added a capability for Leaflet based 2d minimap support using pixi to draw many poi and icons.

---
+++ Frontend/Controllers/PathController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using PPather;
+
+using SharedLib.Converters;
+
+using System.Numerics;
+using System.Text.Json;
+
+namespace Frontend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PathController : ControllerBase
+{
+    private readonly DataConfig dataConfig;
+    private readonly PPatherService service;
+
+    private readonly JsonSerializerOptions options = new()
+    {
+        Converters =
+        {
+            new Vector3Converter(true)
+        }
+    };
+
+    public PathController(DataConfig dataConfig, PPatherService service)
+    {
+        this.dataConfig = dataConfig;
+        this.service = service;
+    }
+
+    [HttpGet()]
+    public IActionResult Get(string filter)
+    {
+        string[] pathFiles = Directory.GetFiles(dataConfig.Path, $"*{filter}*.json", SearchOption.AllDirectories);
+
+        for (int i = 0; i < pathFiles.Length; i++)
+        {
+            string path = pathFiles[i];
+            pathFiles[i] = path.Replace(dataConfig.Root, "");
+        }
+
+        return Ok(pathFiles);
+    }
+
+    [HttpPost("SavePath")]
+    public IActionResult SavePath([FromQuery] string fileName, [FromBody] Vector3[] mapPoints)
+    {
+        string filePath = Path.Join(dataConfig.Path, fileName);
+        System.IO.File.WriteAllText(filePath, JsonSerializer.Serialize(mapPoints, options));
+
+        return Ok();
+    }
+
+    [HttpGet("GetAreaIdAndZ")]
+    [ProducesResponseType(typeof(int), StatusCodes.Status200OK, "application/json")]
+    public IActionResult GetAreaIdAndZ(int mapid, float x, float y)
+    {
+        service.Initialise(mapid);
+
+        (int areaId, float z) = service.GetAreaIdAndZ(new Vector3(x, y, 0));
+        return new JsonResult(new { areaId, z }, options);
+    }
+}
--- Frontend/DependencyInjection.cs
+++ Frontend/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace Frontend;
 
@@ -14,5 +17,42 @@ public static class DependencyInjection
             .AddInteractiveServerComponents();
 
         return services;
+    }
+
+    public static IApplicationBuilder UseCustomStaticFiles(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        DataConfig dataConfig = app.ApplicationServices.GetRequiredService<DataConfig>();
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, dataConfig.Path)),
+            RequestPath = "/path"
+        });
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, dataConfig.Leaflet)),
+            RequestPath = "/tiles"
+        });
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, dataConfig.ExpDbc)),
+            RequestPath = "/dbc"
+        });
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, dataConfig.ExpArea)),
+            RequestPath = "/area"
+        });
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, dataConfig.NpcSpawnLocations)),
+            RequestPath = "/npcspawnlocations"
+        });
+
+        return app;
     }
 }
