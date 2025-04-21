1a789fa13077351c526086e72f6d7d9e08f70d77
Xian55 <367101+Xian55@users.noreply.github.com>
2025-04-21 16:44:45 +0200

    SharedLib: SubZoneArea: A helper class for the upcoming changes for the V1 pathfinder.

---
+++ SharedLib/Data/SubZoneArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Text.Json.Serialization;
+
+namespace SharedLib;
+
+public readonly record struct SubZoneArea
+{
+    public int Id { get; init; }
+    public Vector3 Min { get; init; }
+    public Vector3 Max { get; init; }
+
+    [JsonIgnore]
+    public Vector3 Center => (Min + Max) / 2;
+
+    [JsonIgnore]
+    public float MaxRange => Vector3.DistanceSquared(Min, Max);
+
+    public readonly bool Contains(in Vector3 p)
+    {
+        return
+            p.X >= Min.X && p.X <= Max.X &&
+            p.Y >= Min.Y && p.Y <= Max.Y;
+    }
+}
